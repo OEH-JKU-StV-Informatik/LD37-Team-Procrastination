6da849d1d2eef16df70532f2493d44d8fb4272de
Matthias Holzinger <mh4@xxxlutz.at>
2016-12-10 08:49:47 +0100

    CornerHighlighter

---
+++ Assets/Scripts/CornerHighlightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerHighlightController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += new Vector3(0.2F, 0.2F, 0.2F);
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    private void OnMouseOver()
+    {
+
+    }
+}
