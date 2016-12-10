a69be886861ff48a1fb9759242195e9e938bf3d9
woikerl <alexander.woike@gmail.com>
2016-12-10 05:50:42 +0100

    game logic in model

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int fieldHeight = 5;
+    public int fieldWidth = 5;
+
+    private FieldType[,] field;
+
+    // Use this for initialization
+    void Start()
+    {
+        field = new FieldType[fieldWidth, fieldHeight];
+
+        instantiateField();
+
+    }
+
+    private void instantiateField()
+    {
+        for (int x = 0; x < field.GetLength(1); x++)
+            for (int y = 0; y < field.GetLength(0); y++)
+            {
+                var ft = field[x, y];
+
+                if (ft == FieldType.Empty)
+                {
+                    // Instantiate();
+                }
+            }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/Scripts/SquareDirections.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SquareDirections
+{
+    North, East, South, West
+}
