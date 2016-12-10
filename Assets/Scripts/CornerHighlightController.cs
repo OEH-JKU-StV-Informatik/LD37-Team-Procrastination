45487cbee83b021ee5a15c96eb3571e60e0ea649
Matthias Holzinger <mh4@xxxlutz.at>
2016-12-10 13:23:36 +0100

    Dragging Walls

--- Assets/Scripts/CornerHighlightController.cs
+++ Assets/Scripts/CornerHighlightController.cs
@@ -9,60 +9,110 @@ public class CornerHighlightController : MonoBehaviour
     public float animationStep = 0.006f;
     public float animationRange = 0.08f;
 
+    public Material standard;
+    public Material valid;
+    public Material invalid;
+
+    private bool dragging = false;
     private static CornerHighlightController selected;
     private float animationDirection = 1;
     private float animationSize = 1;
+    private GameObject newWall;
+    private Renderer[] renderers;
 
     // Use this for initialization
     void Start()
     {
-
+        renderers = GetComponentsInChildren<Renderer>();
+        SetMaterial(standard);
     }
 
     // Update is called once per frame
     void Update()
     {
         // onmousemove -> move wall
+        if (dragging)
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, 1000, 1 << LayerMask.NameToLayer("Floor")))
+            {
+                newWall.transform.LookAt(hitInfo.point);
+                newWall.transform.localScale = new Vector3(1, 1, (hitInfo.point - transform.position).magnitude);
+            }
 
+            if (this != selected)
+            {
+                if ((selected.transform.position - transform.position).magnitude <= 1.1)
+                {
+                    selected.SetMaterial(valid);
+                }
+                else
+                {
+                    selected.SetMaterial(invalid);
+                }
+
+            }
+
+        }
     }
 
     void OnMouseEnter()
     {
-//        transform.localScale += Vector3.one * animationRange;
-        animationDirection = 1;
-    }
-
-    void OnMouseDown()
-    {
         selected = this;
-        //Instantiate(wallPrototype, position, Quaternion.Euler(0, 270, 0), gameObject.transform)
-
-        //Instantiate(draggableWallPrototype, playingField, playingField.transform);
-    }
-
-    void OnMouseUp()
-    {
-        if (selected != null && selected != this)
-        {
-            Debug.Log(this.transform.position + " <> " + selected.transform.position);
-            // fix wall
-        }
-
-        selected = null;
-        transform.localScale = Vector3.one;
+        animationDirection = 1;
     }
 
     void OnMouseExit()
     {
-        if (selected != this)
+        if (selected)
+        {
+            selected.SetMaterial(standard);
+        }
+        selected = null;
+        if (!dragging)
             transform.localScale = Vector3.one;
+    }
+
+    void OnMouseUp()
+    {
+        if (dragging)
+        {
+            if (selected)
+            {
+                newWall.transform.LookAt(selected.transform.position);
+                newWall.transform.localScale = Vector3.one;
+                selected.SetMaterial(standard);
+                // fix wall
+                // check if path possible
+            }
+            else
+            {
+                Destroy(newWall);
+            }
+        }
+
+
+        dragging = false;
+        selected = null;
+        transform.localScale = Vector3.one;
+    }
+
+    void OnMouseDown()
+    {
+        dragging = true;
+        //Instantiate(wallPrototype, position, Quaternion.Euler(0, 270, 0), gameObject.transform)
+
+        newWall = Instantiate(draggableWallPrototype);
+        newWall.transform.position = transform.position;
+        newWall.transform.localScale = Vector3.zero;
     }
 
     void OnMouseOver()
     {
-        if (selected == this)
+        if (dragging)
         {
-            transform.localScale = Vector3.one * (1+animationRange);
+            transform.localScale = Vector3.one * (1 + animationRange);
         }
         else
         {
@@ -76,10 +126,18 @@ public class CornerHighlightController : MonoBehaviour
         }
     }
 
+
     void OnMouseDrag()
     {
         transform.localScale = Vector3.one * (1 + animationRange);
     }
 
-    
+    private void SetMaterial(Material material)
+    {
+        Debug.Log("ChangeColorTo " + material.name);
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material = material;
+        }
+    }
 }
