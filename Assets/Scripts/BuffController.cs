8a65fffea814fbd9553373282b64bc917f73111f
Matthias Holzinger <mh4@xxxlutz.at>
2016-12-10 20:53:01 +0100

    Formatting

--- Assets/Scripts/BuffController.cs
+++ Assets/Scripts/BuffController.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuffController : MonoBehaviour {
+public class BuffController : MonoBehaviour
+{
 
     public GameObject playingField;
     public GameObject buffTriggerPrototype;
     public GameObject slowBuffPrototype;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public GameObject placeBuffTrigger(Vector3 position, GameObject buff)
     {
