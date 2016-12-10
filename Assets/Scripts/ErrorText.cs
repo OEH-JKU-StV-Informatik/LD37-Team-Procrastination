952544e5cdfcdfc18492f8b388b221f788b824f5
Bernhard Öhlinger <bernhard@oehli.at>
2016-12-10 15:49:57 +0100

    added error textbox

---
+++ Assets/Scripts/ErrorText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorText : MonoBehaviour {
+
+    public float defaultTextDisplayTime = 5.0f;
+    public Text errorTextField;
+
+    private float timeLeft = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0.0f)
+        {
+            errorTextField.text = "";
+        }
+	}
+
+    public void DisplayError(string message)
+    {
+        DisplayError(message, defaultTextDisplayTime);
+    }
+
+    public void DisplayError(string message, float seconds)
+    {
+        errorTextField.text = message;
+        timeLeft = seconds;
+    }
+}
