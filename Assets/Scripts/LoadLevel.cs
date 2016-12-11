eefa89430a1f820a72de092d8b20c06600a77f49
Simon Stix <simon.stix@gmail.com>
2016-12-11 02:55:02 +0100

    level choose screen added

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public int level = 1;
+
+	// Use this for initialization
+	public void loadScene () {
+		SceneManager.LoadScene("Levels/Level_"+level);
+	}
+}
---
+++ Assets/Scripts/ToggleLevelUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLevelUI : MonoBehaviour {
+
+    private bool state = false; 
+	// Use this for initialization
+	public void ToggleUI () {
+        gameObject.SetActive(state);
+        state = !state;
+	}
+}
