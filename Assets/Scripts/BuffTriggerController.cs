4df6f1a9597fd893632da0dff64531f8c5612cd3
Bernhard Öhlinger <bernhard@oehli.at>
2016-12-11 13:37:09 +0100

    fixed some buff stuff

--- Assets/Scripts/BuffTriggerController.cs
+++ Assets/Scripts/BuffTriggerController.cs
@@ -7,23 +7,33 @@ public class BuffTriggerController : MonoBehaviour
     public GameObject buff;
     private float countDown = -1;
     private EnemyController enemy;
-
+    private Game game;
     // Use this for initialization
     void Start()
     {
-
+        game = Resources.FindObjectsOfTypeAll<Game>()[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!game.IsGameRunning() && enemy != null)
+        {
+            countDown = -1;
+            buff.GetComponent<Buff>().UnDoAction(enemy);
+            GetComponent<Collider>().enabled = true;
+            enemy = null;
+            return;
+        }
+
         if (countDown > 0)
         {
             countDown -= Time.deltaTime;
             if (countDown <= 0)
             {
                 buff.GetComponent<Buff>().UnDoAction(enemy);
-                GetComponent<Collider>().enabled = false;
+                GetComponent<Collider>().enabled = true;
+                enemy = null;
             }
         }
     }
