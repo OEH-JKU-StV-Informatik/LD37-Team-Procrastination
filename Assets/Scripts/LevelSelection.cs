b31b6642bad8b4b98483f5e15b6e12a9dca9000f
Bernhard Öhlinger <bernhard@oehli.at>
2016-12-11 15:22:04 +0100

    level selction scripts

---
+++ Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour {
+
+    public Text levelText;
+
+    public LevelGenerator levelGenerator;
+
+    public int minLevel = 1;
+    public int maxLevel = 3;
+
+	// Use this for initialization
+	void Start () {
+        UpdateLevelText();
+	}
+
+    void UpdateLevelText()
+    {
+        levelText.text = levelGenerator.selectedLevel.ToString();
+    }
+
+    public void NextLevel()
+    {
+        if (levelGenerator.selectedLevel < maxLevel)
+        {
+            levelGenerator.selectedLevel++;
+        }
+        UpdateLevelText();
+    }
+
+    public void PreviousLevel()
+    {
+        if (levelGenerator.selectedLevel > minLevel)
+        {
+            levelGenerator.selectedLevel--;
+        }
+        UpdateLevelText();
+    }
+
+}
