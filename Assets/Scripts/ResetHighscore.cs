29d7cb6b8cfb69ae042dc6ba7d9b1f2d586decd8
Astavinu <astavinu@gmx.at>
2016-12-11 20:16:52 +0100

    per level highscore; not_used Scripts folder

---
+++ Assets/Scripts/ResetHighscore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetHighscore : MonoBehaviour {
+
+    public void resetHighscore()
+    {
+        PlayerPrefs.SetFloat("Highscore_" + SceneManager.GetActiveScene().name, 0f);
+    }
+}
