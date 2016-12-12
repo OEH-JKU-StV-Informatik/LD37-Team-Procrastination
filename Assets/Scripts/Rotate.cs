14de1da61714639e179ae5a34e5f7880dd2edfd1
Simon Stix <simon.stix@gmail.com>
2016-12-12 20:20:57 +0100

    wall count display

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    gameObject.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 60 * Time.deltaTime,0);
+	}
+}
