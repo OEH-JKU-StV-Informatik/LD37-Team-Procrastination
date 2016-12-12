9bcf84378a5d8cbd89920d50d80bb65b302e836a
Matthias Holzinger <mh4@xxxlutz.at>
2016-12-12 03:34:28 +0100

    Position & Size of Highscore, Duration of Errormessage

--- Assets/Scripts/ErrorText.cs
+++ Assets/Scripts/ErrorText.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ErrorText : MonoBehaviour {
+public class ErrorText : MonoBehaviour
+{
 
     public float defaultTextDisplayTime = 5.0f;
     public Text errorTextField;
 
     private float timeLeft = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0.0f)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
         {
-            gameObject.SetActive(false);
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0.0f)
+            {
+                gameObject.SetActive(false);
+            }
         }
-	}
+    }
 
     public void DisplayError(string message)
     {
